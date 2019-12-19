03c32d7adba30961693e45b70e7845a18e4d8c86
felix <felix@newbit.global>
2019-12-19 21:54:22 +0800

    go test switch

--- hello.go
+++ hello.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello," + name
+const spanish = "Spanish"
+const french = "French"
+const chinese = "Chinese"
+const helloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const chineseHelloPrefix = "你好, "
 
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case chinese:
+		prefix = chineseHelloPrefix
+	default:
+		prefix = helloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,43 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello,Chris"
-
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Felix", "")
+		want := "Hello, Felix"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in chinese", func(t *testing.T) {
+		got := Hello("Elodie", "Chinese")
+		want := "你好, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
